b7b151fa5805becb43fb0bfe6db3d23c283dcb90
Halê Valente Silva <halegal@gmail.com>
2016-09-14 17:47:10 -0300

    Gravidade e comentarios adicionados

--- advento_test.py
+++ advento_test.py
@@ -1,39 +1,64 @@
 from FGAme import *
 
+# Mundo
 world = World()
 
-char1 = Circle(20, pos=(35, 35), color='random', mass=10)
+# Personagem
+char1 = Circle(20, pos=(35, 35), color='random', mass=80)
+char1.inertia /= 2
 
+# Terreno
 terrain = AABB(shape=(800, 20), pos=(400, 10), mass='inf')
 
-vec_gravity = AABB(shape=(800, 20), pos=(0, 500), mass='inf')
+# Barra de teste
+heigh_bar = AABB(shape=(50, 2), pos=(400, 150), mass='inf')
 
-# char1.force = lambda t: (0, -10000)
-# k = char1.mass
-# char1.force = lambda t: -k * vec_gravity.pos
-# damping = 0.1
+# Forca de atracao (gravidade)
+char1.gravity = 1000
 
+# Adicionando elementos
 world.add(char1)
+world.add(heigh_bar)
 world.add(terrain)
+world.add.margin(0, 0, 0, 0)
 
-world.add.margin(0, 0, 3, 0)
 
-
+# Mecanica de movimentacao pra direita
 @listen('long-press', 'right')
 def move_right():
     char1.move(6, 0)
+    # O personagem pode perder o controle no jogo por causa de impactos,
+    # esse sera o metodo do player retoma-lo.
+    if char1.vel.x != 0:
+        char1.vel = (0, char1.vel.y)
 
 
+# Mecanica de movimentacao pra esquerda
 @listen('long-press', 'left')
 def move_left():
     char1.move(-6, 0)
+    # O personagem pode perder o controle no jogo por causa de impactos,
+    # esse sera o metodo do player retoma-lo.
+    if char1.vel.x != 0:
+        char1.vel = (0, char1.vel.y)
 
 
+# Mecanica do pulo
 @listen('key-down', 'space')
 def jump():
-    char1.vel = (char1.vel.x, 75)
-    if char1.pos.y > 50:
-        char1.vel = (char1.vel.x, -75)
+    # Já deixando preparado pra uma mecanica de pulo duplo pro jogo
+    # if jump_count < 2:\
+    char1.vel = (char1.vel.x, 275)
 
 
 world.run()
+
+# Junk Code
+#
+# k = char1.mass
+# char1.force = lambda t: (0, -10000)
+# char1.force = lambda t: -k * vec_gravity.pos
+# damping = 0.1
+#
+# if char1.pos.y > 100:
+#       char1.vel = (char1.vel.x, -75)
