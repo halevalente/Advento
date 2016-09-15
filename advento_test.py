581d4280669fe1cf63da9947aaab795bcac2a8c9
Halê Valente Silva <halegal@gmail.com>
2016-09-14 21:49:37 -0300

    Correções menores de design

--- advento_test.py
+++ advento_test.py
@@ -14,15 +14,17 @@ char1.restitution = 0
 # Terreno
 terrain = AABB(shape=(800, 20), pos=(400, 10), mass='inf')
 
-# Barra de teste
-heigh_bar = AABB(shape=(50, 2), pos=(400, 150), mass='inf')
+# Plataformas
+heigh_bar1 = AABB(shape=(115, 2), pos=(300, 75), mass='inf')
+heigh_bar2 = AABB(shape=(50, 2), pos=(200, 50), mass='inf')
 
 # Forca de atracao (gravidade)
 char1.gravity = 2400
 
 # Adicionando elementos ao mundo
 world.add(char1)
-world.add(heigh_bar)
+world.add(heigh_bar1)
+world.add(heigh_bar2)
 world.add(terrain)
 world.add.margin(0)
 
@@ -48,7 +50,7 @@ def move_left():
 
 
 # Mecanica do pulo
-@listen('key-down', 'space')
+@listen('key-down', 'up')
 def jump():
     # Mecanica de pulo duplo pro jogo
     if jump_count < 2:
@@ -57,6 +59,7 @@ def jump():
         jump_count += 1
 
 
+# Reset de pulos
 @listen('frame-enter')
 def update():
     if char1.pos.y < 35:
@@ -66,6 +69,6 @@ def update():
 
 # @listen('pre-collision')
 # def on_collision(col):
-#     print(col)
+#     print('bateu')
 
 world.run()
