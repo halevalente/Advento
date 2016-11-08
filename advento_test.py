48b23b4ef143ad99306cc424194009e18c968c2c
Halê Valente Silva <halegal@gmail.com>
2016-11-08 18:06:55 -0200

    Alterações menores em testes

--- advento_test.py
+++ advento_test.py
@@ -14,18 +14,22 @@ char1.restitution = 0
 
 # Inimigo
 enemy1 = RegularPoly(
-    3, length=75, pos=(750, 35), color='red', mass='5000')
+    3, length=150, pos=(400, 300), color='red', mass='5000')
 enemy1.inertia = 'inf'
 enemy1.restitution = 0
 enemy1.omega += 35
+enemy1.vel = vel.random()
 
 # Terreno
-terrain = AABB(shape=(800, 20), pos=(400, 10), mass='inf')
+terrain = AABB(shape=(800, 20), pos=(400, 10), mass='inf', color='green')
 
 # Plataformas
 platform1 = AABB(shape=(115, 2), pos=(300, 75), mass='inf')
 platform2 = AABB(shape=(50, 2), pos=(200, 50), mass='inf')
 platform3 = AABB(shape=(50, 2), pos=(400, 50), mass='inf')
+platform4 = AABB(shape=(115, 2), pos=(650, 75), mass='inf')
+platform5 = AABB(shape=(50, 2), pos=(550, 50), mass='inf')
+platform6 = AABB(shape=(50, 2), pos=(750, 50), mass='inf')
 
 # Forca de atracao (gravidade)
 char1.gravity = 2400
@@ -36,6 +40,9 @@ world.add(enemy1)
 world.add(platform1)
 world.add(platform2)
 world.add(platform3)
+world.add(platform4)
+world.add(platform5)
+world.add(platform6)
 world.add(terrain)
 world.add.margin(0)
 
@@ -75,7 +82,7 @@ def shot_left():
     shot = world.add.aabb(
         shape=(2, 3),
         pos=(char1.pos.x, char1.pos.y+10),
-        vel=(0, 400),
+        vel=(0, 600),
         mass='inf')
 
 
@@ -85,7 +92,7 @@ def shot_left():
     shot = world.add.aabb(
         shape=(3, 2),
         pos=(char1.pos.x-10, char1.pos.y),
-        vel=(-400, 0),
+        vel=(-600, 0),
         mass='inf')
 
 
@@ -95,21 +102,22 @@ def shot_right():
     shot = world.add.aabb(
         shape=(3, 2),
         pos=(char1.pos.x+10, char1.pos.y),
-        vel=(400, 0),
+        vel=(600, 0),
         mass='inf')
 
 
 # Comando de especial
-@listen('key-down', 'return')
+@listen('key-down', 'space')
 def special_move():
     if special_count < 2:
         blast_count = 0
         char1.inertia = 'inf'
         while blast_count < 10:
-            shot = Circle(5, pos=(char1.pos.x, char1.pos.y), mass='inf')
+            shot = Circle(5, pos=(char1.pos.x, char1.pos.y+25), mass='inf')
             shot.vel = vel.random()
             world.add(shot)
             blast_count += 1
+        char1.inertia /= 2
 
     elif special_count == 2:
         if enemy1.pos.x < char1.pos.x:
@@ -117,7 +125,7 @@ def special_move():
                         pos=(char1.pos.x - 35, char1.pos.y),
                         mass='inf')
             shot.vel = ((enemy1.pos.x - char1.pos.x),
-                        (enemy1.pos.y - char1.pos.y)) # Ainda quebrado
+                        (enemy1.pos.y - char1.pos.y)) # Ainda meio quebrado
 
         elif enemy1.pos.x > char1.pos.x:
             shot = Circle(50,
@@ -125,14 +133,14 @@ def special_move():
                         char1.pos.y),
                         mass='inf')
             shot.vel = ((enemy1.pos.x - char1.pos.x),
-                        (enemy1.pos.y - char1.pos.y)) # Ainda quebrado
+                        (enemy1.pos.y - char1.pos.y)) # Ainda meio quebrado
 
         else:
             shot = Circle(50,
                         pos=(char1.pos.x, char1.pos.y+35),
                         mass='inf')
             shot.vel = ((enemy1.pos.x - char1.pos.x),
-                        (enemy1.pos.y - char1.pos.y)) # Ainda quebrado
+                        (enemy1.pos.y - char1.pos.y)) # Ainda meio quebrado
 
         # shot.vel = (
         #         ((enemy1.pos.x - char1.pos.x)/abs(enemy1.pos.x - char1.pos.x))*500,
@@ -164,6 +172,9 @@ def enemy_heigh():
         enemy1.move(0, -10)
     else:
         enemy1.move(0, 10)
+
+    if enemy1.y >= 450:
+        enemy1.vel += (0,-enemy1.vel.y-150)
 
 
 # Movimentacao do inimigo
