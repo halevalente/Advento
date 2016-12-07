7da7e057b0257109c51480df014c7bd26551cb5b
Vinicius Oliveira <vinifladf@gmail.com>
2016-12-07 11:20:08 -0200

    Menu

--- src/advento.py
+++ src/advento.py
@@ -1,5 +1,15 @@
-from FGAme import *
-from battlefield import *
+# from FGAme import *
+# from battlefield import *
+# from
+#
+# if __name__ == '__main__':
+#     battlefield = Battlefield()
 
-if __name__ == '__main__':
-    battlefield = Battlefield()
+
+#! /usr/bin/env python
+from menuAdvento import *
+
+#inicia o programa .
+pygame.init()
+
+mainMenu()
